deb488a770c11a0ab973d02f07869f2a75072ac5
github username <github email address>
2019-04-07 09:36:15 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,23 @@
+class Song
+
+attr_accessor :title, :artist
+
+# @@all = []
+#
+#   def initialize(title)
+#     @title = title
+#     @artist = artist
+#     @@all << self
+#   end
+#
+#   def self.all
+#     binding.pry
+#     @@all
+#   end
+#
+#   def artist
+#     self
+#   end
+#
+#
+end
